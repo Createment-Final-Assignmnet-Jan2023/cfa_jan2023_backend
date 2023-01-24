7a3a1669ac30c268482623aa1daf7bde16524537
Ohkwari <jjassie@gmail.com>
2023-01-24 16:26:23 +0100

    fleshed out mapper added WebConfig

--- src/main/java/com/manager/cfa_jan2023/service/mapper/PokemonMapper.java
+++ src/main/java/com/manager/cfa_jan2023/service/mapper/PokemonMapper.java
@@ -5,18 +5,36 @@ import com.manager.cfa_jan2023.service.dto.PokemonDTO;
 
 public class PokemonMapper {
     public static PokemonDTO toDto(Pokemon entity) {
-        //todo: fill out rest of fields
         return PokemonDTO.builder()
                 .id(entity.getId())
                 .name(entity.getName())
+                .types(entity.getTypes())
+                .imageFront(entity.getImageFront())
+                .imageBack(entity.getImageBack())
+                .imageArt(entity.getImageArt())
+                .baseHealthPoints(entity.getBaseHealthPoints())
+                .baseNormalAttack(entity.getBaseNormalAttack())
+                .baseNormalDefense(entity.getBaseNormalDefense())
+                .baseSpecialAttack(entity.getBaseSpecialAttack())
+                .baseSpecialDefense(entity.getBaseSpecialDefense())
+                .baseSpeed(entity.getBaseSpeed())
                 .build();
     }
 
     public static Pokemon toEntity(PokemonDTO userDTO) {
-        //todo: fill out rest of fields
         return Pokemon.builder()
                 .id(userDTO.getId())
                 .name(userDTO.getName())
+                .types(userDTO.getTypes())
+                .imageFront(userDTO.getImageFront())
+                .imageBack(userDTO.getImageBack())
+                .imageArt(userDTO.getImageArt())
+                .baseHealthPoints(userDTO.getBaseHealthPoints())
+                .baseNormalAttack(userDTO.getBaseNormalAttack())
+                .baseNormalDefense(userDTO.getBaseNormalDefense())
+                .baseSpecialAttack(userDTO.getBaseSpecialAttack())
+                .baseSpecialDefense(userDTO.getBaseSpecialDefense())
+                .baseSpeed(userDTO.getBaseSpeed())
                 .build();
     }
 }

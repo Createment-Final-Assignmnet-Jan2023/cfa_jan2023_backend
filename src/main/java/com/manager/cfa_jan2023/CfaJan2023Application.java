45896e5fe91f4bfea31a258a38d6b3a3bf5fdd03
Ohkwari <jjassie@gmail.com>
2023-01-24 10:02:53 +0100

    Created the repository

---
+++ src/main/java/com/manager/cfa_jan2023/CfaJan2023Application.java
@@ -0,0 +1,13 @@
+package com.manager.cfa_jan2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfaJan2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfaJan2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/manager/cfa_jan2023/CfaJan2023ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.manager.cfa_jan2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfaJan2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}

905b6bb8d815669feffd59b0e969d46129c01d73
Ohkwari <jjassie@gmail.com>
2023-01-26 11:37:31 +0100

    added 1v1 battle logic

--- src/main/java/com/manager/cfa_jan2023/CfaJan2023Application.java
+++ src/main/java/com/manager/cfa_jan2023/CfaJan2023Application.java
@@ -1,18 +1,18 @@
 package com.manager.cfa_jan2023;
 
-import com.manager.cfa_jan2023.repository.PokemonRepository;
-import com.manager.cfa_jan2023.repository.model.Pokemon;
+import com.manager.cfa_jan2023.service.utils.Battler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static com.manager.cfa_jan2023.Battler.battle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class CfaJan2023Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CfaJan2023Application.class, args);
-
 	}
 
 }
